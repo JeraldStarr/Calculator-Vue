b70fe7aa4300f60ec582ee40ba04d436f5842128
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2022-08-26 00:16:26 +0200

    Created and registered Keyboard component

--- src/App.vue
+++ src/App.vue
@@ -3,34 +3,17 @@
     <section class="calculator__display">
         100
     </section>
-    <section class="calculator__keyboard">
-      <div class="calculator__numbers">
-        <div class="calculator__number">1</div>
-        <div class="calculator__number">2</div>
-        <div class="calculator__number">3</div>
-        <div class="calculator__number">4</div>
-        <div class="calculator__number">5</div>
-        <div class="calculator__number">6</div>
-        <div class="calculator__number">7</div>
-        <div class="calculator__number">8</div>
-        <div class="calculator__number">9</div>
-        <div class="calculator__number">0</div>
-        <div class="calculator__number">,</div>
-      </div>
-      <div class="calculator__operations">
-        <div class="calculator__addition calculator__operation">+</div>
-        <div class="calculator__substraction calculator__operation">-</div>
-        <div class="calculator__divistion calculator__operation">/</div>
-        <div class="calculator__multiplication calculator__operation">x</div>
-      </div>
-    </section>
+  <Keyboard/>
   </div>
 </template>
 
 <script>
-
+import Keyboard from '../components/Keyboard.vue'
 export default {
   name: 'App',
+  components: {
+    Keyboard,
+  }
 }
 </script>
 
@@ -64,39 +47,6 @@ export default {
       padding: 0 25px;
       border-bottom: 1px solid $border-color;
       font-size: 100px;
-    }
-    &__keyboard {
-      display: flex;
-      width: 100%;
-      padding: 8px;
-    }
-    &__numbers {
-      display: grid;
-      grid-template-columns: 120px 120px 120px;
-      row-gap: 20px;
-    }
-    &__number {
-      width: 100px;
-      height: 50px;
-      line-height: 50px;
-      text-align: center;
-      border: 1px solid $border-color;
-      border-radius: 2px;
-      font-size: 30px;
-    }
-    &__operations {
-      display: grid;
-      grid-template-columns: 120px;
-      row-gap: 20px;
-    }
-    &__operation {
-      width: 120px;
-      height: 50px;
-      line-height: 50px;
-      text-align: center;
-      border: 1px solid $border-color;
-      border-radius: 2px;
-      font-size: 30px;
     }
   }
 
