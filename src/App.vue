0c324d81d8a302994ef1098672e568c2bdf64c64
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2022-08-25 23:53:30 +0200

    Added basic style for all elements

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="calculator">
     <section class="calculator__display">
-
+        100
     </section>
     <section class="calculator__keyboard">
-      <div class="numbers"></div>
-      <div class="operation">
-        <div class="addition">+</div>
-        <div class="substraction">-</div>
-        <div class="divistion">/</div>
-        <div class="multiplication">x</div>
+      <div class="calculator__numbers">
+        <div class="calculator__number">1</div>
+        <div class="calculator__number">2</div>
+        <div class="calculator__number">3</div>
+        <div class="calculator__number">4</div>
+        <div class="calculator__number">5</div>
+        <div class="calculator__number">6</div>
+        <div class="calculator__number">7</div>
+        <div class="calculator__number">8</div>
+        <div class="calculator__number">9</div>
+        <div class="calculator__number">0</div>
+        <div class="calculator__number">,</div>
+      </div>
+      <div class="calculator__operations">
+        <div class="calculator__addition calculator__operation">+</div>
+        <div class="calculator__substraction calculator__operation">-</div>
+        <div class="calculator__divistion calculator__operation">/</div>
+        <div class="calculator__multiplication calculator__operation">x</div>
       </div>
     </section>
   </div>
@@ -22,7 +34,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  $border-color: #000;
   *{
     margin: 0;
     padding: 0;
@@ -35,24 +48,59 @@ export default {
     justify-content: center;
     width: 100vw;
     height: 100vh;
+    font-family: Arial, sans-serif;
   }
   .calculator {
     display: flex;
     flex-direction: column;
     width: 500px;
-    height: 700px;
-    border: 1px solid blue;
+    border: 1px solid $border-color;
+    border-radius: 2px;
+    &__display {
+      width: 100%;
+      height: 100px;
+      line-height: 100px;
+      text-align: right;
+      padding: 0 25px;
+      border-bottom: 1px solid $border-color;
+      font-size: 100px;
+    }
+    &__keyboard {
+      display: flex;
+      width: 100%;
+      padding: 8px;
+    }
+    &__numbers {
+      display: grid;
+      grid-template-columns: 120px 120px 120px;
+      row-gap: 20px;
+    }
+    &__number {
+      width: 100px;
+      height: 50px;
+      line-height: 50px;
+      text-align: center;
+      border: 1px solid $border-color;
+      border-radius: 2px;
+      font-size: 30px;
+    }
+    &__operations {
+      display: grid;
+      grid-template-columns: 120px;
+      row-gap: 20px;
+    }
+    &__operation {
+      width: 120px;
+      height: 50px;
+      line-height: 50px;
+      text-align: center;
+      border: 1px solid $border-color;
+      border-radius: 2px;
+      font-size: 30px;
+    }
   }
 
-  .calculator__display {
-    width: 100%;
-    height: 40px;
-    border: 1px solid gray;
-  }
-  .calculator__keyboard {
-    width: 100%;
-    flex-grow: 1;
-    border: 1px solid red;
-  }
+
+
 
 </style>
