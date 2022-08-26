06392f122d2441872abf0a7cf6c43dfe47e90f90
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2022-08-27 00:06:42 +0200

    Added Numbers component

--- components/Keyboard.vue
+++ components/Keyboard.vue
@@ -1,18 +1,6 @@
 <template>
     <section class="calculator__keyboard">
-      <div class="calculator__numbers">
-        <div class="calculator__number">1</div>
-        <div class="calculator__number">2</div>
-        <div class="calculator__number">3</div>
-        <div class="calculator__number">4</div>
-        <div class="calculator__number">5</div>
-        <div class="calculator__number">6</div>
-        <div class="calculator__number">7</div>
-        <div class="calculator__number">8</div>
-        <div class="calculator__number">9</div>
-        <div class="calculator__number">0</div>
-        <div class="calculator__number">,</div>
-      </div>
+      <Numbers/>
       <div class="calculator__operations">
         <div class="calculator__addition calculator__operation">+</div>
         <div class="calculator__substraction calculator__operation">-</div>
@@ -23,9 +11,12 @@
 </template>
 
 <script>
-
+import Numbers from "./Numbers.vue";
 export default {
-   name: 'Keyboard', 
+   name: 'Keyboard',
+   components: {
+    Numbers,
+   }
 }
 </script>
 
@@ -36,20 +27,6 @@ $border-color: #000;
       display: flex;
       width: 100%;
       padding: 8px;
-    }
-    &__numbers {
-      display: grid;
-      grid-template-columns: 120px 120px 120px;
-      row-gap: 20px;
-    }
-    &__number {
-      width: 100px;
-      height: 50px;
-      line-height: 50px;
-      text-align: center;
-      border: 1px solid $border-color;
-      border-radius: 2px;
-      font-size: 30px;
     }
     &__operations {
       display: grid;
---
+++ components/Numbers.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="calculator__numbers">
+        <div class="calculator__number">1</div>
+        <div class="calculator__number">2</div>
+        <div class="calculator__number">3</div>
+        <div class="calculator__number">4</div>
+        <div class="calculator__number">5</div>
+        <div class="calculator__number">6</div>
+        <div class="calculator__number">7</div>
+        <div class="calculator__number">8</div>
+        <div class="calculator__number">9</div>
+        <div class="calculator__number">0</div>
+        <div class="calculator__number">,</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Numbers",
+}
+</script>
+
+<style lang="scss" scoped>
+    $border-color: #000;
+    .calculator {
+        &__numbers {
+        display: grid;
+        grid-template-columns: 120px 120px 120px;
+        row-gap: 20px;
+    }
+        &__number {
+        width: 100px;
+        height: 50px;
+        line-height: 50px;
+        text-align: center;
+        border: 1px solid $border-color;
+        border-radius: 2px;
+        font-size: 30px;
+        }
+    }
+</style>
